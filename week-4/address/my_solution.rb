2af0112acecfa4fa1bff511a80745d4300b68875
Shea Munion <sheamunion@gmail.com>
2015-10-20 16:15:40 -0500

    Remove test address from solution

--- week-4/address/my_solution.rb
+++ week-4/address/my_solution.rb
@@ -7,5 +7,3 @@
 def make_address(street, city, state, zip)
   puts "You live at " + street + ", in the beautiful city of " + city + ", " + state + ". Your zip is " + zip.to_s + "."
 end
-
-make_address("715 Ridge Rd", "Wilmette", "IL", "60091")
