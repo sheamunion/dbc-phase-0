d79ab3183861d91af0495ceeee58b03fabefc111
Shea Munion <sheamunion@gmail.com>
2015-10-21 17:48:21 -0500

    Add 4.5.2 files

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,21 @@
+# Calculate a Grade
+
+# I worked on this challenge [with: Michael Whelpley].
+
+
+# Your Solution Below
+
+
+def get_grade(grade)
+  if grade >= 90
+    p "A"
+  elsif grade >=80
+    p "B"
+  elsif grade >=70
+    p "C"
+  elsif grade >= 60
+    p "D"
+  else
+    p "F"
+  end
+end
