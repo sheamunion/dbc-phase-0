5b6c4e4069003dcc288b7d642f13ba5b77d03d97
Shea Munion <sheamunion@gmail.com>
2015-10-22 12:57:46 -0500

    Complete 4.7.1 and initialize 4.7.2

---
+++ week-4/factorial/my_solution.rb
@@ -0,0 +1,94 @@
+# Factorial
+
+# I worked on this challenge [with James Artz].
+
+
+# Your Solution Below
+def factorial(number)
+  if number == 0
+    return 1
+  else
+    i = number-1
+    while i >= 1
+      number = number * i
+      i = i - 1
+    end
+    return number
+  end
+end
+
+
+
+
+# Find our pseudocode below
+
+
+# Input: number
+# Output: number that is the factorial of the input
+
+# multiplication step: Take the input number and multiply input number by (itself minus 1)
+
+# run the multiplication step until the input number equals 1
+
+# output the product of the multiplication step
+
+
+#   input is 6
+#   x is output
+
+
+#   x = input * input - 1
+
+#   x = x * input - 2
+
+#   x = x * input - 3
+
+#   (do this until it reaches (input -1))
+
+#   p x
+
+#     until input == 1
+
+#     while input >= 1
+
+# def factorial(num)
+#   a = 1
+#   while num >= a do
+#     x = num * (num -= 1)
+
+#   end
+# p x
+
+# end
+
+
+
+# def factorial(number)
+#   x = 1
+#   i = number
+
+#   while i >= 1
+
+#     x = x * (number * i)
+#     i = i - 1
+#     p x
+#   end
+
+
+
+# end
+
+
+
+# factorial(5)
+
+# for i in 1..50
+#     print i
+# end
+
+
+# i = 1
+# while i <= 50 do
+#     print i
+#     i += 1
+# end
