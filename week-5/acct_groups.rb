35395a2646d48bff7e447d57592800296faf9368
Shea Munion <sheamunion@gmail.com>
2015-10-29 21:53:00 -0500

    Refactor initial solution

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -9,15 +9,14 @@ STEPS:
       **HOW DO I DETERMINE THE NUMBER OF GROUPS?
         **Number (INTEGER) of people in the list divided by 3 with no remainder (can't have part of a person) (we want at least three people in any group) THIS tells me how many groups to create
         => If we have ten people, we should have three groups. i.e. 10/3 = 3
-      **I want to assign each person a group number. Create a hash where key = name and value = group number. We should only have three possible group numbers (assuming 10 people)
-
-
+      **I want to assign each person a group number. Create a hash where key = name and value = group number.
   RETURN OUTPUT
   CREATE INPUT
   INVOKE the process and pass it INPUT
-
-====== RELEASE 2: INITIAL SOLUTION ======
 =end
+
+#====== RELEASE 2: INITIAL SOLUTION ======
+
 list_of_people = [
   "James",
   "Adwoa",
@@ -48,7 +47,8 @@ def create_groups(people)
   p names_groups
 end
 
-#DRIVER TEST CODE
+#======  RELEASE 5: DRIVER TEST CODE ======
+
 p create_groups(list_of_people) == {
   "James"=>1,
   "Adwoa"=>2,
@@ -57,22 +57,9 @@ p create_groups(list_of_people) == {
   "Dan"=>1,
   "Kevin"=>2,
   "Hassan"=>1}
-=begin
-====== RELEASE 3: ADD COMPLEXITY ======
-=end
 
-# WHAT IF TWO OR MORE PEOPLE HAVE THE SAME NAME? Then their names cannot be the key in our hash. Their names must be the values, the group numbers must be the keys.
-#For every group number, append x names to that group number's value
-
-list_of_people = [
-  "James",
-  "Adwoa",
-  "David",
-  "Jalal",
-  "James",
-  "Kevin",
-  "Hassan"]
-
+# ====== RELEASE 4: INITIAL SOLUTION REFACTORED ======
+# (Rearrange the output hash so that keys are the group numbers and the values are strings of people's names. Also, clean up output.)
 def create_groups(people)
   number_people = people.length
   if number_people < 3
@@ -80,41 +67,28 @@ def create_groups(people)
   else
     group_quantity = number_people / 3
   end
-  names_groups = people.map.with_index { |group, name| [0, name] }.to_h
-#   group_number = 1
-#   names_groups.each do |name, group|
-#     if group_number <= group_quantity
-#       names_groups[name] = group_number
-#       group_number += 1
-#     else
-#       names_groups[name] = 1
-#       group_number = 2
-#     end
-#   end
-  p names_groups
+  group_number = 1
+  groups_names = Hash.new("")
+  people.each do |name|
+    if group_number <= group_quantity
+      groups_names[group_number] += (name + ", ")
+      group_number += 1
+    else
+      groups_names[1] += (name + ", ")
+      group_number = 2
+    end
+  end
+  groups_names.each do |group, name|
+    print "\n", "Group ", group, "\n"
+    print name,  "\n"
+  end
 end
 
-#DRIVER TEST CODE
-p create_groups(list_of_people)
-# == {
-#     "1"=>["James","David","James","Hassan"]
-#     "2"=>["Adwoa","Jalal","Kevin"]
-#     }
-
-
 =begin
-====== RELEASE 4: REFACTOR ======
+====== RELEASE 3: ADD COMPLEXITY ======
 
-=end
-//code here//
 
-=begin
-====== RELEASE 5: DRIVER CODE ======
 
-=end
-//code here//
-
-=begin
 ====== RELEASE 6: REFLECTION ======
 What was the most interesting and most difficult part of this challenge?
 
